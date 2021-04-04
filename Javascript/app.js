8db0087e93c46790a2d44aba92011c06badbf811
Ayyoub Al keyyam <ayoub.alqeam@gmail.com>
2021-04-04 17:17:37 +0300

    add the typeof to console

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,11 +1,11 @@
 let nickName =prompt('Enter your Nickname !')
-console.log(nickName)
+console.log(typeof nickName)
 let age =prompt('How old are you ?')
-console.log(age)
+console.log(typeof age)
 let device =prompt('What device do you usually use?')
-console.log(device)
+console.log(typeof device)
 let favOs =prompt('What is your favorite OS ?')
-console.log(favOs)
+console.log(typeof favOs)
 if (confirm('Let me guess some information about you !!')) {
     alert('Hello ' + nickName +' I think your age is ' + age + ' And you are using ' + device + ' And you like the ' + favOs + ' system. This is the information that you have entered :D')
 }
